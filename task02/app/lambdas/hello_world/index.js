6a68a1ed1827ae35ab75d401b88d866982988f1f
Dmytro <dkalitovskiy@gmail.com>
2024-10-09 17:53:54 +0300

    Final

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -8,18 +8,13 @@ exports.handler = async (event) => {
         // Повертаємо успішну відповідь
         return {
             statusCode: 200,
-            body: JSON.stringify({
-                message: 'Hello from Lambda'
-            }),
+            message: 'Hello from Lambda',
         };
     } else {
         // Повертаємо помилку 400 Bad Request для будь-якого іншого шляху або методу
         return {
             statusCode: 400,
-            body: JSON.stringify({
-                message: `Bad request syntax or unsupported method. Request path: ${requestPath}. HTTP method: ${httpMethod}`,
-            }),
+            message: `Bad request syntax or unsupported method. Request path: ${requestPath}. HTTP method: ${httpMethod}`,
         };
     }
 };
-
