519cb2511e279b3ebe3880e082afb73ca4dc4abf
Dmytro <dkalitovskiy@gmail.com>
2024-10-09 17:37:48 +0300

    3 attempt

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,8 +1,22 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        message: ('Hello from Lambda!'),
-    };
-    return response;
+    // Extract the request path and HTTP method from the event object
+    const requestPath = event.path;
+    const httpMethod = event.httpMethod;
+
+    // Check if the request path is '/hello'
+    if (requestPath === '/hello' && httpMethod === 'GET') {
+        // Return the success response
+        return {
+            statusCode: 200,
+            message: 'Hello from Lambda',
+        };
+    } else {
+        // Return the 400 Bad Request response for any other path or method
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: `Bad request syntax or unsupported method. Request path: ${requestPath}. HTTP method: ${httpMethod}`,
+            }),
+        };
+    }
 };
