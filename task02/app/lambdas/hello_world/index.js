d4b6842ac8f6fac2850f0a50d123bbb63c3fc54e
Dmytro <dkalitovskiy@gmail.com>
2024-10-09 18:18:51 +0300

    corrected status code

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,22 +1,24 @@
 exports.handler = async (event) => {
-    // Отримуємо шлях та метод з об'єкта event
+
     const requestPath = event.rawPath;
     const httpMethod = event.requestContext.http.method;
 
-    // Перевіряємо, чи є шлях '/hello'
+
     if (requestPath === '/hello' && httpMethod === 'GET') {
-        // Повертаємо успішну відповідь
+
         return {
             statusCode: 200,
             body: {
+                statusCode: 200,
                 message: 'Hello from Lambda'
             },
         };
     } else {
-        // Повертаємо помилку 400 Bad Request для будь-якого іншого шляху або методу
+
         return {
             statusCode: 400,
             body: {
+                statusCode: 400,
                 message: `Bad request syntax or unsupported method. Request path: ${requestPath}. HTTP method: ${httpMethod}`,
             },
         };
