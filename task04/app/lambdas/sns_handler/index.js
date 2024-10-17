284d0ef1769199868f21c909b610707a6dab599b
Dmytro <dkalitovskiy@gmail.com>
2024-10-17 12:46:05 +0300

    add sns

--- task04/app/lambdas/sns_handler/index.js
+++ task04/app/lambdas/sns_handler/index.js
@@ -1,8 +1,5 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    event.Records.forEach(record => {
+        console.log('SNS Message:', record.Sns.Message);
+    });
 };
