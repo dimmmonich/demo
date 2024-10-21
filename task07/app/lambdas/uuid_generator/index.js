5b754b3d5144c9b040cdb0778ffdf83c9fd91448
Dmytro <dkalitovskiy@gmail.com>
2024-10-21 14:52:18 +0300

    indet

--- task07/app/lambdas/uuid_generator/index.js
+++ task07/app/lambdas/uuid_generator/index.js
@@ -1,33 +1,28 @@
 const AWS = require('aws-sdk');
 const S3 = new AWS.S3();
 const { v4: uuidv4 } = require('uuid');
-let lastInvocationTime = null;
 
 exports.handler = async (event) => {
     const currentTime = new Date();
 
-    lastInvocationTime = currentTime;
-    const timestamp = currentTime.toISOString();
-
-
+    // Format the timestamp to avoid invalid characters
+    const formattedTimestamp = currentTime.toISOString().replace(/:/g, '-');
     const uuids = [];
+
     for (let i = 0; i < 10; i++) {
         uuids.push(uuidv4());
     }
 
-
     const fileContent = JSON.stringify({ ids: uuids }, null, 2);
 
-
     const params = {
-        Bucket: process.env.BUCKET_NAME,
-        Key: timestamp,
+        Bucket: process.env.BUCKET_NAME, // Ensure this environment variable is set
+        Key: `uuids-${formattedTimestamp}.json`, // Use formatted timestamp as filename
         Body: fileContent,
         ContentType: 'application/json'
     };
 
     try {
-
         await S3.putObject(params).promise();
         console.log(`File successfully uploaded to S3: ${params.Key}`);
     } catch (error) {
