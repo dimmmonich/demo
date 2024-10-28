dc5a5131f540805eb257709c59d501a70e1e0f52
Dmytro <dkalitovskiy@gmail.com>
2024-10-28 12:49:18 +0200

    task08 sdk

--- task08/app/lambdas/api_handler/index.js
+++ task08/app/lambdas/api_handler/index.js
@@ -1,4 +1,11 @@
-const OpenMeteoClient = require('/opt/nodejs/open-meteo-sdk/OpenMeteoClient'); // /opt is the path for layers
+const path = require('path');
+
+// Перевіряємо, чи виконується код в Lambda середовищі
+const OpenMeteoClient = require(
+    process.env.LAMBDA_TASK_ROOT
+        ? '/opt/nodejs/open-meteo-sdk/OpenMeteoClient'
+        : path.join(__dirname, '..', '..', 'nodejs', 'open-meteo-sdk', 'OpenMeteoClient')
+);
 
 exports.handler = async (event) => {
     const api = new OpenMeteoClient();
